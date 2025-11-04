29b606b683d0132a36637c582bbe2bd8fb0025ef
Denis Panasyuk <den.dzim@yandex.ru>
2025-11-04 13:35:20 +0300

    Реализован функционал согласно ТЗ

--- src/test/java/ru/yandex/practicum/filmorate/controller/InMemoryUserStorageTest.java
+++ src/test/java/ru/yandex/practicum/filmorate/controller/InMemoryUserStorageTest.java
@@ -3,7 +3,6 @@ package ru.yandex.practicum.filmorate.controller;
 import org.junit.jupiter.api.Test;
 import ru.yandex.practicum.filmorate.exception.ValidationException;
 import ru.yandex.practicum.filmorate.model.User;
-import ru.yandex.practicum.filmorate.service.UserService;
 import ru.yandex.practicum.filmorate.storage.InMemoryUserStorage;
 
 import java.time.LocalDate;
@@ -11,10 +10,8 @@ import java.time.LocalDate;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertThrows;
 
-public class UserControllerTest  {
-    InMemoryUserStorage inMemoryUserStorage;
-    UserService userService;
-    UserController userController = new UserController(inMemoryUserStorage, userService);
+public class InMemoryUserStorageTest  {
+    InMemoryUserStorage inMemoryUserStorage = new InMemoryUserStorage();
 
     @Test
     void getFilmTest() {
@@ -23,17 +20,17 @@ public class UserControllerTest  {
         user.setLogin("test2");
         user.setBirthday(LocalDate.of(2000, 12, 27));
         user.setEmail("test@");
-        userController.create(user);
+        inMemoryUserStorage.create(user);
         user.setEmail("  ");
-        assertThrows(ValidationException.class, () -> userController.update(user));
+        assertThrows(ValidationException.class, () -> inMemoryUserStorage.update(user));
         user.setEmail("test2@");
         user.setLogin("TEST ETS");
-        assertThrows(ValidationException.class, () -> userController.update(user));
+        assertThrows(ValidationException.class, () -> inMemoryUserStorage.update(user));
         user.setLogin("test2");
         user.setName(" ");
-        userController.update(user);
+        inMemoryUserStorage.update(user);
         assertEquals(user.getLogin(), user.getName(), "Имя совпадает с логином");
         user.setBirthday(LocalDate.of(2200, 12, 27));
-        assertThrows(ValidationException.class, () -> userController.update(user));
+        assertThrows(ValidationException.class, () -> inMemoryUserStorage.update(user));
     }
 }
