db63eedd85afb441951b9677a5b6e2fdf31d6610
Denis Panasyuk <den.dzim@yandex.ru>
2025-11-04 17:41:16 +0300

    Добавлен метод для обработки всех оставшихся исключений

--- src/main/java/ru/yandex/practicum/filmorate/controller/ErrorHandler.java
+++ src/main/java/ru/yandex/practicum/filmorate/controller/ErrorHandler.java
@@ -22,4 +22,10 @@ public class ErrorHandler {
     public ErrorResponse handleNotFoundException(final NotFoundException e) {
         return new ErrorResponse(e.getMessage());
     }
+
+    @ExceptionHandler
+    @ResponseStatus(HttpStatus.INTERNAL_SERVER_ERROR)
+    public ErrorResponse handleAllOtherExceptions(Throwable e) {
+        return new ErrorResponse(e.getMessage());
+    }
 }
