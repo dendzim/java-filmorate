4a4fe4bfbd94c81e3e0182e5e23de9fa86a26cd0
Denis Panasyuk <den.dzim@yandex.ru>
2025-12-25 00:05:14 +0300

    Внесены правки согласно замечаниям

--- src/main/java/ru/yandex/practicum/filmorate/model/ErrorResponse.java
+++ src/main/java/ru/yandex/practicum/filmorate/model/ErrorResponse.java
@@ -4,7 +4,7 @@ import lombok.Getter;
 
 @Getter
 public class ErrorResponse {
-    String error;
+    private final String error;
 
     public ErrorResponse(String error) {
         this.error = error;
